717813f0adc04dabdceb828b798259c26b982f20
Cherkashin Alexander <cherkalexander@gmail.com>
2024-08-25 17:51:43 +0300

    reading time in Russian

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -5,6 +5,7 @@ export function remarkReadingTime() {
 	return function (tree: any, { data }: any) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
-		data.astro.frontmatter.minutesRead = readingTime.text;
+		const displayText = Math.ceil(readingTime.minutes) + ' мин';
+		data.astro.frontmatter.minutesRead = displayText;
 	};
 }
